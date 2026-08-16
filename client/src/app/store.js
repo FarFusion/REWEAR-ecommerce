c7cdbf30d3d3a02ca038cfae75a6a8c071c05448
FarFusion <mdfarhan650@gmail.com>
2026-08-16 05:43:59 +0530

    updated hardcoded client url

--- client/src/app/store.js
+++ client/src/app/store.js
@@ -1,13 +1,3 @@
-// import { configureStore } from "@reduxjs/toolkit";
-
-// import authReducer from "../features/auth/authSlice";
-
-// export const store = configureStore({
-//   reducer: {
-//     auth: authReducer,
-//   },
-// });
-
 
 import { configureStore } from "@reduxjs/toolkit";
 
