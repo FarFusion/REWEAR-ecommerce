393fccddc62a810e5703b9c8712b6c74e27c389f
FarFusion <mdfarhan650@gmail.com>
2026-08-20 03:55:35 +0530

    updated smtp cofiguration again

--- server/src/utils/sendEmail.js
+++ server/src/utils/sendEmail.js
@@ -9,6 +9,10 @@ const transporter = nodemailer.createTransport({
   host: "smtp.gmail.com",
   port: 587,
   secure: false,
+  requireTLS: true,
+  connectionTimeout: 10000,
+  greetingTimeout: 10000,
+  socketTimeout: 10000,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
